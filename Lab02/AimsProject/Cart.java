7086ee2b42753e0c9f8416bb6e6a21bdc3bd032a
betty <huynh.dh2310@gmail.com>
2022-11-10 15:51:45 +0700

    feat(*): initial commit

---
+++ Lab02/AimsProject/Cart.java
@@ -0,0 +1,40 @@
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        qtyOrdered++;
+        if (qtyOrdered <= MAX_NUMBER_ORDERED) {
+            int i = qtyOrdered - 1;
+            itemsOrdered[i] = disc;
+            System.out.println("The disc has been added!");
+        } else {
+            System.out.println("The disc is almost full!");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered == 0) {
+            System.out.println("The cart have 0 dict");
+        } else {
+            qtyOrdered--;
+            for (int i = 0; i < itemsOrdered.length; ++i) {
+                if (disc.equals(itemsOrdered[i])) {
+
+                }
+            }
+        }
+    }
+
+    public float totalCost() {
+        float sum = 0.0f;
+        int i = 0;
+        while (itemsOrdered[i] != null) {
+            sum += itemsOrdered[i].getCost();
+            ++i;
+        }
+        return sum;
+    }
+}
