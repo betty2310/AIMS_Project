1ed12858b15d62e70754a8f944fa899ac205ab26
betty <huynh.dh2310@gmail.com>
2022-11-24 14:00:03 +0700

    delete trash code

--- AimsProject/TestPassingParameter.java
+++ AimsProject/TestPassingParameter.java
@@ -12,18 +12,9 @@ public class TestPassingParameter {
     public static void swap(Object o1, Object o2) {
         DigitalVideoDisc a = (DigitalVideoDisc) o1;
         DigitalVideoDisc b = (DigitalVideoDisc) o2;
-        DigitalVideoDisc tmp = new DigitalVideoDisc(a.getTitle(), a.getCategory(), a.getDirector(), a.getLength(),
-                a.getCost());
+        DigitalVideoDisc tmp = new DigitalVideoDisc(a.getTitle());
         a.setTitle(b.getTitle());
-        a.setCategory(b.getCategory());
-        a.setDirector(b.getDirector());
-        a.setCost(b.getCost());
-        a.setLength(b.getLength());
         b.setTitle(tmp.getTitle());
-        b.setCategory(tmp.getCategory());
-        b.setDirector(tmp.getDirector());
-        b.setCost(tmp.getCost());
-        b.setLength(tmp.getLength());
     }
 
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
